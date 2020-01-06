ffb3d1beff66fc3597f35a28a2357bfc2927275a
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-05 23:25:30 -0300

    ASP.NET Core 3.1 + Custom Middleware + SQL Server
    ASP.NET Core 3.1 + Custom Middleware + SQL Server

---
+++ ExemploAPIMiddleware/Controllers/TestesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploAPIMiddleware.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ASP.NET Core 3.1", "Teste com Middleware Customizado"};
+        }
+    }
+}
